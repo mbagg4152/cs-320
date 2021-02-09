7027d8f7ba7d06437b25dc4a355603199d8d0453
mag <mag@buntu>
2021-02-09 16:14:56 -0500

    Update Assignment2_Maggie_Horton.zip, assignment2.html, and 3 more files...

--- Assignments/A2/lib/a2.js
+++ Assignments/A2/lib/a2.js
@@ -1,8 +1,13 @@
 // Maggie Horton Winter 2021
+// TODO - CHANGE VAR NAMES
+// TODO - PROB DONT USE SNAKE CASE NAMES
 
 /********************************************************
  * Shaders
  ********************************************************/
+
+// TODO - just write these bad boys using regular string concatenation or smth im just a particular bitch and thought it was ugly
+// TODO - also the shaders dont need to be in screaming snake case do w/e u want
 let SRC_VERT = code_lines(['attribute vec4 a_pos;',
                            'void main(){',
                            '    gl_Position = a_pos;',
@@ -19,6 +24,10 @@ let SRC_FRAG = code_lines(['precision mediump float;',
 /********************************************************
  * JS code
  ********************************************************/
+
+
+// TODO - could have a separate let statement on each line. also could opt out of using global vars and modify functions to pass values
+// TODO - also could change let to var i think?
 let
     wgl_ctx,
     canvas,
@@ -32,6 +41,8 @@ let
     color_array;
 
 let points = [];
+
+// TODO - i dont think you need to have this bitch be called main. just whatever you change it to, make sure to update it in the html to have this launch on load
 function main() {
     // document elements
     canvas = elem('main_canvas');
@@ -39,22 +50,25 @@ function main() {
     picker_input = elem('color_picker');
     picker_lbl = elem('color_lbl');
     pt_count = 0;
-    color_hex = '#1a7bd5';
+    color_hex = '#1a7bd5'; // TODO - CHANGE COLOR VALUE
     picker_input.value = color_hex;
     picker_btn.style.backgroundColor = color_hex;
-    init_webgl();
+    init_webgl(); // TODO - should just initialize values here i just felt it was too cluttered. that or maybe combine w/ register_events
 
-    try { color_array = make_vec4_color(); } catch (e) { alert(e); }
+    try { color_array = make_vec4_color(); } catch (e) { alert(e); } // TODO - could be moved i think? just makes vec4 array from color
 
-    register_events();
+    register_events(); // TODO - can register here or just combine w/ init_webgl
 
     // clear everything
     wgl_ctx.clearColor(1.0, 1.0, 1.0, 1.0);
     wgl_ctx.clear(wgl_ctx.COLOR_BUFFER_BIT);
+
+    // TODO - idk if this line needs to go here
     wgl_ctx.uniform4f(attr_color, color_array[0], color_array[1], color_array[2], color_array[3]);
 }
 
 function init_webgl() { // set up webgl context, shaders, etc. and make sure program is loaded
+    // TODO - prob dont need try-catch
     try { wgl_ctx = getWebGLContext(canvas); } catch (e) { alert('ERR - WEBGL CONTEXT'); }
     try { initShaders(wgl_ctx, SRC_VERT, SRC_FRAG); } catch (e) { alert('ERR - SHADERS'); }
     try { attr_pos = wgl_ctx.getAttribLocation(wgl_ctx.program, 'a_pos'); } catch (e) { alert('ERR - POS'); }
@@ -64,14 +78,16 @@ function init_webgl() { // set up webgl context, shaders, etc. and make sure pro
 function register_events() { // register events for canvas, button and color picker
     canvas.onclick = function (event_left) { left_click(event_left); }
     canvas.oncontextmenu = function (event_right) { right_click(event_right); }
+    // TODO - remove this if you dont add button
     picker_btn.onclick = function () { color_click(); }
     picker_input.onclick = function () { get_color(); }
     picker_input.onchange = function () { get_color(); }
 }
 
 function left_click(press) {
-    pt_count += 1;
+    pt_count += 1; // TODO - not needed, can use points.length/2 later when point count is needed
 
+    // TODO - idk how to change this that much but pls try ur darndest
     // get coords based on canvas size & then convert to webgl coords
     let x = (press.offsetX / canvas.clientWidth) * 2 - 1;
     let y = ((canvas.clientHeight - press.offsetY) / canvas.clientHeight) * 2 - 1;
@@ -79,16 +95,22 @@ function left_click(press) {
     // add mouse click points to point array
     points.push(x);
     points.push(y);
+    // TODO - prob dont need try catch?
     try {update_canvas(wgl_ctx, points, attr_pos, pt_count, attr_color, color_array);} catch (e) {alert('ERR UPDATING CANVAS\n' + e);}
 }
 
 function right_click(press) {
+    // TODO - get rid of x & y idk why i left them and submitted the code with these in here ugh
     let x = (press.offsetX / canvas.clientWidth) * 2 - 1;
     let y = ((canvas.clientHeight - press.offsetY) / canvas.clientHeight) * 2 - 1;
+    // TODO - maybe can remove this? idk how itll affect it if popup still happens
     press.preventDefault(); // prevent right-click context menu from popping up
     points.splice(0, points.length); // clear points from mouse click
 
+    // TODO - could also just reinforce that points.length is 0 idk
     pt_count = 0;
+
+    // TODO - prob dont need try catch
     try {update_canvas(wgl_ctx, points, attr_pos, pt_count, attr_color, color_array);} catch (e) {alert('ERR UPDATING CANVAS\n' + e);}
 }
 
@@ -98,9 +120,11 @@ function color_click() {
 
 function get_color() {
     color_hex = picker_input.value;
-    picker_btn.style.backgroundColor = color_hex;
-    canvas.style.border = '7px solid ' + color_hex;
+    picker_btn.style.backgroundColor = color_hex; // TODO - dont need
+    canvas.style.border = '7px solid ' + color_hex; // TODO - dont need
     let vec4 = make_vec4_color();
+
+    // TODO - extra bitch shit
     if (is_bright(vec4)) {
         picker_btn.style.color = '#151414';
     } else {
@@ -108,6 +132,6 @@ function get_color() {
     }
     color_array = vec4;
     update_canvas(wgl_ctx, points, attr_pos, pt_count, attr_color, vec4);
-    update_style([picker_input, picker_lbl], 'inline-block', 'none');
+    update_style([picker_input, picker_lbl], 'inline-block', 'none'); // TODO - dont need
 }
 
