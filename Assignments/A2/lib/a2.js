f4675337dec9e78ec27cd91ee4fc65546f5658f0
mag <mag@buntu>
2021-02-09 11:46:32 -0500

    Update assignment2.html, a2.js, and 3 more files...

--- Assignments/A2/lib/a2.js
+++ Assignments/A2/lib/a2.js
@@ -1,80 +1,109 @@
-function elem(id) {return document.getElementById(id);}
-
 /********************************************************
  * Shaders
  ********************************************************/
-const NLS = '\n;', S_SRC = 'void main(){\n', E_SRC = '}\n';
+let SRC_VERT = code_lines(['attribute vec4 a_pos;',
+                           'void main(){',
+                           '  gl_Position = a_pos;',
+                           '  gl_PointSize = 2.0;',
+                           '}']);
 
-let SRC_VERT =
-    'attribute vec4 a_pos' + NLS +
-    S_SRC +
-    '  gl_Position = a_pos' + NLS +
-    '  gl_PointSize =2.0' + NLS +
-    E_SRC;
-
-let SRC_FRAG = 'void main() {\n' +
-    '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
-    '}\n';
+let SRC_FRAG = code_lines(['',
+                           'void main() {',
+                           '  gl_FragColor = vec4(1.0, 0.55, 0.17, 1.0);',
+                           '}']);
 
 /********************************************************
  * JS code
  ********************************************************/
-let gl, canvas, head_xy, head_left, head_right, cnt_right, cnt_left, a_pos, points = [];
+let gl, canvas, head_xy, head_btn, cnt_btn, cnt_left, a_pos, line_color, color_btn, picker, lbl, points = [], color;
 
 function main() {
+
     // document elements
     canvas = elem('main_canvas');
     head_xy = elem('head_xy');
-    head_left = elem('head_left');
-    head_right = elem('head_right');
-    cnt_right = 0;
+    head_btn = elem('head_btn');
+    color_btn = elem('color_btn');
+    picker = elem('color_picker');
+    lbl = elem('color_lbl');
+    cnt_btn = 0;
     cnt_left = 0;
+    color = '#450825';
+    init_webgl();
 
-    // set up webgl context, shaders and make sure program is loaded
-    try { gl = getWebGLContext(canvas); } catch (e) { alert('PROBLEM LOADING WEBGL CONTEXT\n' + e); }
-    try { initShaders(gl, SRC_VERT, SRC_FRAG) } catch (e) { alert('ERR LOADING SHADERS\n' + e); }
-    try { a_pos = gl.getAttribLocation(gl.program, 'a_pos'); } catch (e) { alert('NO LOC FOR A_POS\n' + e); }
+    let vec4_start;
+    try { vec4_start = make_vec4_color(); } catch (e) { alert(e); }
+    vec4_start.push(0.0);
 
-    // register events for left & right mouse clicks
-    canvas.onclick = function (event_left) { left_click(event_left); }
-    canvas.oncontextmenu = function (event_right) { right_click(event_right); }
+    register_events();
 
     // clear everything
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
-function left_click(event_left) {
+function init_webgl() { // set up webgl context, shaders, etc. and make sure program is loaded
+    try { gl = getWebGLContext(canvas); } catch (e) { alert('PROBLEM LOADING WEBGL CONTEXT\n' + e); }
+    try { initShaders(gl, SRC_VERT, SRC_FRAG); } catch (e) { alert('ERR LOADING SHADERS\n' + e); }
+    try { a_pos = gl.getAttribLocation(gl.program, 'a_pos'); } catch (e) { alert('NO LOC FOR A_POS\n' + e); }
+}
+
+function register_events() { // register events for canvas, button and color picker
+    canvas.onclick = function (event_left) { left_click(event_left); }
+    canvas.oncontextmenu = function (event_right) { right_click(event_right); }
+    color_btn.onclick = function (press) { color_click(press); }
+    picker.onclick = function (press) { get_color(press); }
+    picker.onchange = function (press) { get_color(press); }
+}
+
+function left_click(press) {
     // get coords based on canvas size & then convert to webgl coords
-    let x = (event_left.offsetX / canvas.clientWidth) * 2 - 1;
-    let y = ((canvas.clientHeight - event_left.offsetY) / canvas.clientHeight) * 2 - 1;
+    let x = (press.offsetX / canvas.clientWidth) * 2 - 1;
+    let y = ((canvas.clientHeight - press.offsetY) / canvas.clientHeight) * 2 - 1;
 
     // add mouse click points to point array
     points.push(x);
     points.push(y);
 
-    head_xy.innerText = '(' + x + ',' + y + ')'; // for testing, remove later
     cnt_left += 1; // for testing, remove later
-    head_left.innerText = 'left click count: ' + cnt_left; // for testing, remove later
+    head_xy.innerText = ' left click count: ' + cnt_left + ' ~~~~ x,y: (' + x.toFixed(3) + ', ' + y.toFixed(3) + ')'; // for testing, remove later
+    try {update_canvas(gl, points, a_pos, cnt_left);} catch (e) {alert('ERR UPDATING CANVAS\n' + e);}
 
-    update_points();
 }
 
-function right_click(event_right) {
-    event_right.preventDefault(); // prevent right-click context menu from popping up
+function right_click(press) {
+    let x = (press.offsetX / canvas.clientWidth) * 2 - 1;
+    let y = ((canvas.clientHeight - press.offsetY) / canvas.clientHeight) * 2 - 1;
+    press.preventDefault(); // prevent right-click context menu from popping up
     points = []; // clear points from mouse click
-    update_points();
+
     cnt_left = 0; // for testing, remove later
-    head_left.innerText = 'left click count: ' + cnt_left; // for testing, remove later
-    cnt_right += 1; // for testing, remove later
-    head_right.innerText = 'right click count: ' + cnt_right; // for testing, remove later
+    head_xy.innerText = ' left click count: ' + cnt_left + ' ~~~~ x,y: (' + x.toFixed(3) + ', ' + y.toFixed(3) + ')'; // for testing, remove later
+    try {update_canvas(gl, points, a_pos, cnt_left);} catch (e) {alert('ERR UPDATING CANVAS\n' + e);}
 }
 
-function update_points() { // take points from mouse click and add to canvas
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    let p_len = points.length;
-    for (let i = 0; i < p_len; i += 2) {
-        gl.vertexAttrib3f(a_pos, points[i], points[i + 1], 0.0);
-        gl.drawArrays(gl.POINTS, 0, 1);
-    }
+function color_click(press) {
+    if (lbl.style.display === "none") lbl.style.display = "inline-block";
+    if (picker.style.display === "none") picker.style.display = "inline-block";
+}
+
+function get_color(press) {
+    color = picker.value;
+    make_vec4_color();
+
+    if (lbl.style.display === "inline-block") lbl.style.display = "none";
+    if (picker.style.display === "inline-block") picker.style.display = "none";
+    head_btn.innerText = 'Vec4 color: ' + color;
+}
+
+function make_vec4_color() { // hex --> decimal --> vec4: #RRGGBB --> R,G,B --> (R/255),(G/255),(B/255)
+    color = color.replace('#', '');
+    let as_vec4 = [to_vec4(color, 0, 1), to_vec4(color, 2, 3), to_vec4(color, 0, 1)];
+    color = as_vec4.join(', ');
+    return as_vec4;
+}
+
+function to_vec4(line, idx0, idx1) { // get hex for R, G or B based on index values then divide by 255
+    let hex = [line.charAt(idx1), line.charAt(idx0)].join('');
+    return (parseInt(hex, 16) / 255.0).toFixed(3);
 }
--- Assignments/A2/lib/cuon-utils.js
+++ Assignments/A2/lib/cuon-utils.js
@@ -1,4 +1,44 @@
-/* THIS IS THE UTIL FILE PROVIDED BY THE BOOK */
+/* THIS IS THE UTIL FILE PROVIDED BY THE BOOK
+*  Added some functions */
+
+/********************************************************
+ * Added code
+ ********************************************************/
+function elem(id) { return document.getElementById(id); } // rename function to get document elements
+
+function code_lines(lines) { return lines.join('\n'); } // join code lines by \n, more readable
+
+function update_canvas(gl, points, a_pos, count) { // take points from mouse click and add to canvas
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    if (count === 1) { // need to draw start point of line
+        gl.vertexAttrib3f(a_pos, points[0], points[1], 0.0);
+        gl.drawArrays(gl.POINTS, 0, 1);
+    } else { // update canvas with lines
+        let res;
+        try { res = config_vertex_buffers(gl, a_pos, points, count);} catch (e) { alert('FAILED TO SET POS'); }
+        gl.drawArrays(gl.LINE_STRIP, 0, res);
+    }
+}
+
+function config_vertex_buffers(gl, a_pos, points, count) {
+    let verts = new Float32Array(points);
+    let vert_buff;
+    try {
+        vert_buff = gl.createBuffer()
+    } catch (e) {
+        alert('FAILED MAKING VERT BUFF\n' + e);
+        return -1;
+    }
+    gl.bindBuffer(gl.ARRAY_BUFFER, vert_buff);
+    gl.bufferData(gl.ARRAY_BUFFER, verts, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(a_pos, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_pos);
+    return count;
+}
+
+/********************************************************
+ * Original code
+ ********************************************************/
 if (!window.cancelAnimationFrame) {
     window.cancelAnimationFrame = (window.cancelRequestAnimationFrame ||
         window.webkitCancelAnimationFrame || window.webkitCancelRequestAnimationFrame ||
