315b12f4d898a0b3c1f40709a5d925a374de6307
mag <mag@buntu>
2021-02-16 12:50:34 -0500

    Update cuon-matrix.js, part1.js, and util_consts.js

--- Assignments/A3/lib/part1.js
+++ Assignments/A3/lib/part1.js
@@ -7,7 +7,7 @@ function main() {
     pts_ellipse = make_round_points('E', 0.6, 0.4);
     config_draw(gl1, 'E', can1, make_vec4_color(dark_red), src_vert, src_frag, false);
     if (src_frag_tri === null || src_frag_tri === undefined) alert('triangle frag shader is null :(');
-    config_draw(gl2, 'T', can2, make_vec4_color(dark_blue), src_grad_vert, src_grad_frag, true);
+    config_draw(gl2, 'T', can2, make_vec4_color(dark_blue), src_grad_vert, src_grad_frag, false);
     config_draw(gl3, 'C', can3, make_vec4_color(dark_green), src_vert, src_frag, false);
     config_draw(gl4, 'S', can4, make_vec4_color(black), src_vert, src_frag, false);
 }
@@ -18,6 +18,7 @@ function config_draw(gl, shape, canvas, color_vals, v_shader, f_shader, grad) {
     try { initShaders(gl, v_shader, f_shader, grad); } catch (e) { clog('SHADER ERR\n' + e); }
     try { v_count = init_buffers(gl, shape); } catch (e) { clog('VBUFF ERR\n' + e); }
     try { uni_trans = gl.getUniformLocation(gl.program, 'u_trans'); } catch (e) { clog('TRANS ERR\n' + e); }
+
     if (shape !== 'T') {
         try { uni_color = gl.getUniformLocation(gl.program, 'l_color'); } catch (e) { clog('COLOR ERR\n' + e); }
         gl.uniform4f(uni_color, color_vals[0], color_vals[1], color_vals[2], color_vals[3]);
@@ -32,65 +33,6 @@ function config_draw(gl, shape, canvas, color_vals, v_shader, f_shader, grad) {
     else if (shape === 'C' || shape === 'E') gl.drawArrays(gl.TRIANGLE_STRIP, 0, v_count);
 }
 
-function make_round_points(shape, a, b) {
-    let verts = [];
-    for (let i = 0.0; i <= 360.0; i += 1) {
-        let pt = (i * Math.PI) / 180;
-        if (shape === 'C') verts = verts.concat([Math.cos(pt) / 2, Math.sin(pt) / 2, 0, 0]);
-        if (shape === 'E') verts = verts.concat([(a * Math.cos(pt)), (b * Math.sin(pt)), 0, 0]);
-    }
 
-    return verts;
-}
 
-function init_buffers(gl, shape, grad) {
-    let n = -1, verts;
-    if (shape === 'T') {
-        n = 3;
-        verts = new Float32Array(pts_triangle);
-    } else if (shape === 'S') {
-        n = 6;
-        verts = new Float32Array(pts_square);
-    } else if (shape === 'C') {
-        n = pts_circle.length / 2;
-        verts = new Float32Array(pts_circle);
-    } else if (shape === 'E') {
-        n = pts_ellipse.length / 2;
-        verts = new Float32Array(pts_ellipse);
-    }
 
-    if (grad) {
-        let vert_buff = make_buff(gl, gl.ARRAY_BUFFER, verts, gl.STATIC_DRAW);
-        let color_buff = gl.createBuffer(gl, gl.ARRAY_BUFFER, new Float32Array(tri_colors), gl.STATIC_DRAW);
-        let index_buff = gl.createBuffer(gl, gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(tri_indices), gl.STATIC_DRAW);
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, vert_buff);
-        try { attr_pos = gl.getAttribLocation(gl.program, 'a_pos'); } catch (e) { clog('POS ERR\n' + e); }
-        gl.vertexAttribPointer(attr_pos, 3, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(attr_pos);
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, color_buff);
-        try { attr_color = gl.getAttribLocation(gl.program, 'a_color'); } catch (e) { clog('COLOR ERR\n' + e); }
-        gl.vertexAttribPointer(attr_color, 4, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(attr_color);
-
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buff);
-
-    } else {
-        let vert_buff = make_buff(gl, gl.ARRAY_BUFFER, verts, gl.STATIC_DRAW);
-        if (!vert_buff) clog('Failed to create the buffer object');
-        gl.bindBuffer(gl.ARRAY_BUFFER, vert_buff);
-
-        try { attr_pos = gl.getAttribLocation(gl.program, 'a_pos'); } catch (e) { clog('POS ERR\n' + e);}
-        gl.vertexAttribPointer(attr_pos, 2, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(attr_pos);
-    }
-
-    return n;
-}
-function make_buff(gl, target, bufferArray, usage) {
-    let buffer = gl.createBuffer();
-    gl.bindBuffer(target, buffer);
-    gl.bufferData(target, bufferArray, usage);
-    return buffer;
-}
--- Assignments/A3/lib/util_consts.js
+++ Assignments/A3/lib/util_consts.js
@@ -1,53 +1,12 @@
-function mk_src(lines) { return lines.join('\n'); } // join code lines by \n, more readable
-const src_vert = mk_src(['attribute vec4 a_pos;',
-                         'uniform vec4 u_trans;',
-                         'void main() {',
-                         '  gl_Position = a_pos + u_trans;',
-                         '  gl_PointSize = 10.0;',
-                         '}']);
+/*************************************************************************************************************
+ * Functions - Simple
+ *************************************************************************************************************/
+function clog(text) { console.log(text); } // rename console.log
+function elem(id) { return document.getElementById(id); } // rename document.getElementById
 
-const src_frag = mk_src(['precision mediump float;',
-                         'uniform vec4 l_color;',
-                         'void main() {',
-                         '  gl_FragColor = l_color;',
-                         '}']);
-
-const src_frag_tri = mk_src(['precision mediump float;',
-                             'uniform vec2 u_resolution;',
-                             'void main() {',
-                             '  vec2 st = gl_FragCoord.xy/u_resolution.xy;',
-                             '  vec3 color1 = vec3(1.9,0.55,0);',
-                             '  vec3 color2 = vec3(0.226,0.000,0.615);',
-                             '  float mixValue = distance(st,vec2(0,1));',
-                             '  vec3 color = mix(color1,color2,mixValue);',
-                             '  gl_FragColor = vec4(color,mixValue);',
-                             '}']);
-
-const src_grad_frag = 'varying lowp vec4 v_color;\n' +
-                      'void main(void) {\n' +
-                      ' gl_FragColor = v_color;\n' +
-                      '  }';
-const src_grad_vert = 'attribute vec3 a_pos;\n' +
-                      'attribute vec4 a_color;\n' +
-                      'varying lowp vec4 v_color;\n' +
-                      'void main(void) {\n' +
-                      ' gl_Position = vec4(a_pos, 1.0);\n' +
-                      ' v_color = a_color;\n' +
-                      '  }';
-
-const pts_triangle = [0, 0.5, -0.5, -0.5, 0.5, -0.5];
-const pts_square = [-0.3, -0.3, -0.3, 0.3, 0.3, 0.3, -0.3, -0.3, 0.3, 0.3, 0.3, -0.3];
-const dark_red = '#6a0000';
-const dark_green = '#184200';
-const dark_blue = '#00276a';
-const black = '#000000';
-
-const tri_colors = [1, 0, 0, 1, Math.random(), Math.random(), Math.random(), 1, Math.random(), Math.random(), Math.random(), 1];
-const tri_indices = [0, 1, 2];
-
-function clog(text) { console.log(text); }
-function elem(id) {return document.getElementById(id);}
-
+/*************************************************************************************************************
+ * Functions - Set/Change/Convert values
+ *************************************************************************************************************/
 function make_vec4_color(color_hex) { // hex --> decimal --> vec4: #RRGGBB --> R,G,B --> (R/255),(G/255),(B/255)
     let color_copy = color_hex;
     color_copy = color_copy.replace('#', '');
@@ -58,5 +17,85 @@ function to_vec4(line, idx0, idx1) { // get hex for R, G or B based on index val
     return (parseInt([line.charAt(idx0), line.charAt(idx1)].join(''), 16) / 255.0).toFixed(5);
 }
 
+function make_round_points(shape, a, b) {
+    let verts = [];
+    for (let i = 0.0; i <= 360.0; i += 1) {
+        let pt = (i * Math.PI) / 180;
+        if (shape === 'C') verts = verts.concat([Math.cos(pt) / 2, Math.sin(pt) / 2, 0, 0]);
+        if (shape === 'E') verts = verts.concat([(a * Math.cos(pt)), (b * Math.sin(pt)), 0, 0]);
+    }
+    return verts;
+}
 
+/*************************************************************************************************************
+ * Functions - Dealing with WebGL
+ *************************************************************************************************************/
+function make_buff(gl, arr_type, buff_arr_vals, usage) {
+    let buffer = gl.createBuffer();
+    gl.bindBuffer(arr_type, buffer);
+    gl.bufferData(arr_type, buff_arr_vals, usage);
+    return buffer;
+}
 
+function init_buffers(gl, shape, grad) {
+    let n = -1, verts;
+    if (shape === 'T') {
+        n = 3;
+        verts = new Float32Array(pts_triangle);
+    } else if (shape === 'S') {
+        n = 6;
+        verts = new Float32Array(pts_square);
+    } else if (shape === 'C') {
+        n = pts_circle.length / 2;
+        verts = new Float32Array(pts_circle);
+    } else if (shape === 'E') {
+        n = pts_ellipse.length / 2;
+        verts = new Float32Array(pts_ellipse);
+    }
+
+    if (grad) {
+
+    } else {
+        let vert_buff = make_buff(gl, gl.ARRAY_BUFFER, verts, gl.STATIC_DRAW);
+        if (!vert_buff) clog('Failed to create the buffer object');
+        gl.bindBuffer(gl.ARRAY_BUFFER, vert_buff);
+
+        try { attr_pos = gl.getAttribLocation(gl.program, 'a_pos'); } catch (e) { clog('POS ERR\n' + e);}
+        gl.vertexAttribPointer(attr_pos, 2, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(attr_pos);
+    }
+
+    return n;
+}
+/*************************************************************************************************************
+ * Constants
+ *************************************************************************************************************/
+const src_vert = 'attribute vec4 a_pos; uniform vec4 u_trans; ' +
+                 'void main() { gl_Position = a_pos + u_trans; }'
+
+const src_frag = 'precision mediump float; uniform vec4 l_color; ' +
+                 'void main() { gl_FragColor = l_color; }'
+
+const src_frag_tri = 'precision mediump float; uniform vec2 u_resolution; void main() { ' +
+                     'vec2 st = gl_FragCoord.xy/u_resolution.xy; vec3 color1 = vec3(1.9,0.55,0); ' +
+                     'vec3 color2 = vec3(0.226,0.000,0.615); float mixValue = distance(st,vec2(0,1)); ' +
+                     'vec3 color = mix(color1,color2,mixValue); gl_FragColor = vec4(color,mixValue); }';
+
+const src_grad_frag = 'varying lowp vec4 v_color; void main(void){' +
+                      ' gl_FragColor = v_color; }';
+
+const src_grad_vert = 'attribute vec3 a_pos; attribute vec4 a_color; varying lowp vec4 v_color; ' +
+                      'void main(void){ gl_Position = vec4(a_pos, 1.0); v_color = a_color; }';
+
+const grad_frag = 'precision mediump float; uniform float u_Width; uniform float u_Height;' +
+                  'void main(){ gl_FragColor = vec4(gl_FragCoord.x/u_Width, 0.0, gl_FragCoord.y/u_Height, 1.0); }';
+
+const pts_triangle = [0, 0.5, -0.5, -0.5, 0.5, -0.5];
+const pts_square = [-0.3, -0.3, -0.3, 0.3, 0.3, 0.3, -0.3, -0.3, 0.3, 0.3, 0.3, -0.3];
+const dark_red = '#6a0000';
+const dark_green = '#184200';
+const dark_blue = '#00276a';
+const black = '#000000';
+
+const tri_colors = [1, 0, 0, 1, Math.random(), Math.random(), Math.random(), 1, Math.random(), Math.random(), Math.random(), 1];
+const tri_indices = [0, 1, 2];
