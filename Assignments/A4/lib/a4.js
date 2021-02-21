2a246a0e7773e0cdab00c0b7531433effe6bb876
mag <mag@buntu>
2021-02-21 11:23:02 -0500

    Update a4.html and a4.js

--- Assignments/A4/lib/a4.js
+++ Assignments/A4/lib/a4.js
@@ -1,5 +1,9 @@
 const SRC_VERT = 'attribute vec4 a_pos; uniform vec4 u_trans; void main() { gl_Position = a_pos + u_trans; }';
 const FRAG_BASIC = 'precision mediump float; uniform vec4 u_col;  void main() { gl_FragColor = u_col; }';
+const tbtn_txt1 = 'Translate by 10', tbtn_txt2 = 'Move back', sbtn_txt1 = 'Scale by 2', sbtn_txt2 = 'Original size',
+    rbtn_txt1 = 'Rotate 45 degrees', rbtn_txt2 = 'Rotate back', abtn_txt1 = 'Start animation',
+    abtn_txt2 = 'Stop animation';
+
 const pts = [-0.2, -0.2, -0.2, 0.2, 0.2, 0.2, -0.2, -0.2, 0.2, 0.2, 0.2, -0.2];
 const color = vec4('#8d0000');
 
@@ -12,8 +16,21 @@ let wgl_ctx;
 let uni_color;
 let uni_trans;
 let v_count;
+let btn_translate = elem('btn_translate');
+let btn_scale = elem('btn_scale');
+let btn_rotate = elem('btn_rotate');
+let btn_animate = elem('btn_animate');
 
 function main() {
+    setup_webgl();
+    btn_translate.onclick = function () {trans_click();}
+    btn_scale.onclick = function () {scale_click();}
+    btn_rotate.onclick = function () {rotate_click();}
+    btn_animate.onclick = function () {animate_click();}
+
+}
+
+function setup_webgl() {
     try { wgl_ctx = getWebGLContext(canvas); } catch (e) { alert('CTX ERR\n' + e); }
     try { initShaders(wgl_ctx, SRC_VERT, FRAG_BASIC); } catch (e) { clog('SHADER ERR\n' + e); }
     try { uni_trans = wgl_ctx.getUniformLocation(wgl_ctx.program, 'u_trans'); } catch (e) { clog('TRANS ERR\n' + e); }
@@ -26,3 +43,32 @@ function main() {
     wgl_ctx.drawArrays(wgl_ctx.TRIANGLE_STRIP, 0, v_count);
 }
 
+function trans_click() {
+    if (btn_translate.innerText === tbtn_txt1) btn_translate.innerText = tbtn_txt2;
+    else if (btn_translate.innerText === tbtn_txt2) btn_translate.innerText = tbtn_txt1;
+
+}
+
+function scale_click() {
+    if (btn_scale.innerText === sbtn_txt1) btn_scale.innerText = sbtn_txt2;
+    else if (btn_scale.innerText === sbtn_txt2) btn_scale.innerText = sbtn_txt1;
+}
+
+function rotate_click() {
+    if (btn_rotate.innerText === rbtn_txt1) btn_rotate.innerText = rbtn_txt2;
+    else if (btn_rotate.innerText === rbtn_txt2) btn_rotate.innerText = rbtn_txt1;
+}
+
+function animate_click() {
+    if (btn_animate.innerText === abtn_txt1) {
+        btn_animate.innerText = abtn_txt2;
+        btn_translate.disabled = true;
+        btn_scale.disabled = true;
+        btn_rotate.disabled = true;
+    } else if (btn_animate.innerText === abtn_txt2) {
+        btn_animate.innerText = abtn_txt1;
+        btn_translate.disabled = false;
+        btn_scale.disabled = false;
+        btn_rotate.disabled = false;
+    }
+}
