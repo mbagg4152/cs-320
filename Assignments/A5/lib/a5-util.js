2f3e6e12942db65988f8173e60ed03ecb9fbad92
mag <mag@buntu>
2021-03-02 17:04:03 -0500

    Update a5.html, a5-util.js, and a5.js

--- Assignments/A5/lib/a5-util.js
+++ Assignments/A5/lib/a5-util.js
@@ -1,22 +1,11 @@
-// Maggie Horton - Graphics Assignment 4 Winter 2021
-
-
-function update_canvas(gl, pos_loc, points, u_matrix, m_matrix) { // update canvas w/ points and transformation matrix
-    gl.clearColor(1, 1, 1, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    let res;
-    try { res = init_vbuff(gl, points, pos_loc);} catch (e) { alert('FAILED TO SET POS'); }
-    wgl_ctx.uniformMatrix4fv(u_matrix, false, m_matrix.elements);
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, res);
-}
-
-function vec4(color_hex) { // hex --> decimal --> vec4: #RRGGBB --> R,G,B --> (R/255),(G/255),(B/255)
+// Maggie Horton - Graphics Assignment 5 Winter 2021
+function vec3(color_hex) { // hex --> decimal --> vec4: #RRGGBB --> R,G,B --> (R/255),(G/255),(B/255)
     let color_copy = color_hex;
     color_copy = color_copy.replace('#', '');
-    return [hex_vec4(color_copy, 0, 1), hex_vec4(color_copy, 2, 3), hex_vec4(color_copy, 4, 5), 1.0];
+    return [hex_vec3(color_copy, 0, 1), hex_vec3(color_copy, 2, 3), hex_vec3(color_copy, 4, 5)];
 }
 
-function hex_vec4(line, idx0, idx1) { // get hex for R, G or B based on index values then divide by 255
+function hex_vec3(line, idx0, idx1) { // get hex for R, G or B based on index values then divide by 255
     return (parseInt([line.charAt(idx0), line.charAt(idx1)].join(''), 16)/255.0).toFixed(5);
 }
 
--- Assignments/A5/lib/a5.js
+++ Assignments/A5/lib/a5.js
@@ -1,103 +1,87 @@
-// Maggie Horton - Graphics Assignment 4 Winter 2021
-const SRC_VERT = 'attribute vec4 a_pos;  ' +
-                 'attribute vec4 a_col; ' +
-                 'uniform mat4 u_vm;' +
-                 'uniform mat4 u_pm;' +
-                 'varying vec4 v_col;' +
-                 'void main() { ' +
-                 'gl_Position = u_pm * u_vm * a_pos;  ' +
-                 'v_col = a_col; }';
-const FRAG_BASIC = 'precision mediump float;' +
-                   'varying vec4 v_col;' +
-                   'void main() { ' +
-                   'gl_FragColor = v_col; ' +
-                   '}';
-const POINTS = [-0.2, -0.2,
-                -0.2, 0.2,
-                0.2, 0.2,
-                -0.2, -0.2,
-                0.2, 0.2,
-                0.2, -0.2];
-const COLOR = vec4('#00447d');
+// Maggie Horton - Graphics Assignment 5 Winter 2021
+const SRC_VERT = 'attribute vec4 a_pos;  attribute vec4 a_col;  uniform mat4 u_vm; uniform mat4 u_pm; varying vec4 v_col;' +
+                 'void main() {  gl_Position = u_pm * u_vm * a_pos; v_col = a_col; }';
+const SRC_FRAG = 'precision mediump float; varying vec4 v_col; void main() {  gl_FragColor = v_col; }';
+
+const sk_c1 = vec3('#00a3ff'), sk_c2 = vec3('#1671fa'), sk_c3 = vec3('#6b1efa'), sk_c4 = vec3('#52c9ff'), // sky colors
+    gnd_c1 = vec3('#035c52'), gnd_c2 = vec3('#01660f'), gnd_c3 = vec3('#3c6f03'), // ground colors
+    bt_c1 = vec3('#2d5500'), bt_c2 = vec3('#2a3101'), bt_c3 = vec3('#2a3100'), // background tree colors
+    ft_c1 = vec3('#03804e'), ft_c2 = vec3('#022d49'), ft_c3 = vec3('#022d49'), // foreground tree colors
+    s_c1 = vec3('#ffba03'), s_c2 = vec3('#d14914'); // sun-face colors
 
 let canvas = elem('wgl_canvas');
 let wgl_ctx, v_matrix, p_matrix, a_position, a_color, u_view, u_proj;
-let numVertex = 3, numColor = 3, n = 18, v_count = 0;
-let pc = new Float32Array([
-                              // Three triangles on the right side
-                              0.75, 1.0, -4.0, 0.4, 1.0, 0.4, // The back green one
-                              0.25, -1.0, -4.0, 0.4, 1.0, 0.4,
-                              1.25, -1.0, -4.0, 1.0, 0.4, 0.4,
+let n_vert = 3, n_col = 3, init_vb_res = 0, size_mult = 6; // vals per point, vals per color, result of init buffer
+let tri_vtx = 3, sq_vtx = 6, face_vtx = 30; // v_count for triangle, rect and sun-face
+let z_tr1 = 0.0, z_tr2 = 0.8, z_sky = -6, z_gnd = -6.6, z_sun = -1.7; // z vals for each object
 
-                              0.75, 1.0, -2.0, 1.0, 1.0, 0.4, // The middle yellow one
-                              0.25, -1.0, -2.0, 1.0, 1.0, 0.4,
-                              1.25, -1.0, -2.0, 1.0, 0.4, 0.4,
+// each point has x,y,z,r,g,b values
+let bg_tree_pts = [].concat([0.15, 1.0, z_tr1], bt_c1, [-0.35, -1.0, z_tr1], bt_c2, [0.65, -1.0, z_tr1], bt_c3);
+let fg_tree_pts = [].concat([0.35, 1.0, z_tr2], ft_c1, [-0.15, -1.0, z_tr2], ft_c2, [0.85, -1.0, z_tr2], ft_c3);
 
-                              0.75, 1.0, 0.0, 0.4, 0.4, 1.0,  // The front blue one
-                              0.25, -1.0, 0.0, 0.4, 0.4, 1.0,
-                              1.25, -1.0, 0.0, 1.0, 0.4, 0.4,
+let sky_pts = [].concat([-4.0, -1.37, z_sky], sk_c1, [-4.0, 4.0, z_sky], sk_c2, [4.0, 4.0, z_sky], sk_c3,
+                        [-4.0, -1.37, z_sky], sk_c4, [4.0, 4.0, z_sky], sk_c3, [4.0, -1.37, z_sky], sk_c1);
 
-                              // Three triangles on the left side
-                              -0.75, 1.0, -4.0, 0.4, 1.0, 0.4, // The back green one
-                              -1.25, -1.0, -4.0, 0.4, 1.0, 0.4,
-                              -0.25, -1.0, -4.0, 1.0, 0.4, 0.4,
+let ground_pts = [].concat([4.0, 0.0, z_gnd], gnd_c1, [4.0, -4.0, z_gnd], gnd_c1, [-4.0, -4.0, z_gnd], gnd_c1,
+                           [4.0, 0.0, z_gnd], gnd_c3, [-4.0, -4.0, z_gnd], gnd_c1, [-4.0, 0.0, z_gnd], gnd_c2);
 
-                              -0.75, 1.0, -2.0, 1.0, 1.0, 0.4, // The middle yellow one
-                              -1.25, -1.0, -2.0, 1.0, 1.0, 0.4,
-                              -0.25, -1.0, -2.0, 1.0, 0.4, 0.4,
-
-                              -0.75, 1.0, 0.0, 0.4, 0.4, 1.0,  // The front blue one
-                              -1.25, -1.0, 0.0, 0.4, 0.4, 1.0,
-                              -0.25, -1.0, 0.0, 1.0, 0.4, 0.4,
-                          ]);
-
-let tri1 = new Float32Array([0.0, 0.5, -0.1, 0.0, 0.0, 1.0,
-                             -0.5, -0.5, -0.1, 0.0, 0.0, 1.0,
-                             0.5, -0.5, -0.1, 1.0, 1.0, 0.0,]);
-let tri2 = new Float32Array([0.5, 0.4, -0.5, 1.0, 1.0, 0.0,
-                             -0.5, 0.4, -0.5, 1.0, 0.0, 0.0,
-                             0.0, -0.6, -0.5, 1.0, 0.0, 0.0,]);
+let sun_face_pts = [].concat([-1.0, 0.0, z_sun], s_c1, [-0.7, 0.4, z_sun], s_c1, [0.55, 0.0, z_sun], s_c2,
+                             [-0.75, -0.15, z_sun], s_c1, [-0.65, 0.0, z_sun], s_c1, [-0.65, -0.25, z_sun], s_c1,
+                             [-0.65, -0.4, z_sun], s_c1, [-0.7, -0.3, z_sun], s_c1, [-0.65, -0.25, z_sun], s_c1,
+                             [-0.3, -0.5, z_sun], s_c1, [-0.65, -0.4, z_sun], s_c1, [-0.65, 0.0, z_sun], s_c1,
+                             [-0.7, -0.6, z_sun], s_c1, [-0.65, -0.4, z_sun], s_c1, [-0.3, -0.5, z_sun], s_c1,
+                             [-0.3, -0.5, z_sun], s_c1, [-0.65, 0.0, z_sun], s_c1, [0.55, 0.0, z_sun], s_c2,
+                             [0.0, 0.15, z_sun], s_c2, [0.7, 0.5, z_sun], s_c1, [0.55, 0.0, z_sun], s_c2,
+                             [0.0, 0.15, z_sun], s_c2, [0.3, 0.9, z_sun], s_c1, [0.7, 0.5, z_sun], s_c1,
+                             [0.0, 0.15, z_sun], s_c2, [-0.4, 0.9, z_sun], s_c1, [0.3, 0.9, z_sun], s_c1,
+                             [0.0, 0.15, z_sun], s_c2, [-0.4, 0.9, z_sun], s_c1, [-0.7, 0.4, z_sun], s_c1);
 
 function main() {
     setup_webgl();
-
+    init_vb_res = init_vbuff(wgl_ctx, n_vert, n_col, sky_pts);
+    wgl_ctx.drawArrays(wgl_ctx.TRIANGLE_STRIP, 0, sq_vtx); // draw sky rectangle
+    init_vb_res = init_vbuff(wgl_ctx, n_vert, n_col, sun_face_pts);
+    wgl_ctx.drawArrays(wgl_ctx.TRIANGLES, 0, face_vtx); // draw sun-face
+    init_vb_res = init_vbuff(wgl_ctx, n_vert, n_col, bg_tree_pts);
+    wgl_ctx.drawArrays(wgl_ctx.TRIANGLES, 0, tri_vtx); // draw first tree
+    init_vb_res = init_vbuff(wgl_ctx, n_vert, n_col, fg_tree_pts);
+    wgl_ctx.drawArrays(wgl_ctx.TRIANGLES, 0, tri_vtx); // draw second tree
 }
 
 function setup_webgl() {
     v_matrix = new Matrix4();
     p_matrix = new Matrix4();
     wgl_ctx = getWebGLContext(canvas);
-    initShaders(wgl_ctx, SRC_VERT, FRAG_BASIC);
-    v_count = init_vbuff(wgl_ctx, a_position, a_color, numVertex, numColor, pc); // setup buffer
-    u_view = wgl_ctx.getUniformLocation(wgl_ctx.program, 'u_vm');
-    u_proj = wgl_ctx.getUniformLocation(wgl_ctx.program, 'u_pm');
+    initShaders(wgl_ctx, SRC_VERT, SRC_FRAG);
+    init_vb_res = init_vbuff(wgl_ctx, n_vert, n_col, ground_pts, sq_vtx); // setup buffer
 
-    v_matrix.setLookAt(0, 0, 5, 0, 0, -100, 0, 1, 0);
-    p_matrix.setPerspective(30, canvas.width/canvas.height, 1, 100);
+    u_view = wgl_ctx.getUniformLocation(wgl_ctx.program, 'u_vm'); // uniform view matrix
+    u_proj = wgl_ctx.getUniformLocation(wgl_ctx.program, 'u_pm'); // uniform projection matrix
+    v_matrix.setLookAt(0, 0, 3, 0, 0, -400, 0, 8, 0); // setup view
+    p_matrix.setPerspective(45, canvas.width/canvas.height, 1, 500); // set perspective
+    wgl_ctx.uniformMatrix4fv(u_view, false, v_matrix.elements); // send view to shader
+    wgl_ctx.uniformMatrix4fv(u_proj, false, p_matrix.elements); // send projection to shader
 
-    wgl_ctx.uniformMatrix4fv(u_view, false, v_matrix.elements);
-    wgl_ctx.uniformMatrix4fv(u_proj, false, p_matrix.elements);
-    wgl_ctx.enable(wgl_ctx.DEPTH_TEST);
-    wgl_ctx.clearColor(0.0, 0.0, 0.0, 1.0);
+    wgl_ctx.clearColor(1.0, 1.0, 1.0, 1.0);
     wgl_ctx.clear(wgl_ctx.COLOR_BUFFER_BIT);
-    wgl_ctx.drawArrays(wgl_ctx.TRIANGLES, 0, n);
+    wgl_ctx.drawArrays(wgl_ctx.TRIANGLE_STRIP, 0, sq_vtx); // draw ground rectangle
 }
 
-function init_vbuff(gl, a_pos, a_col, vert_num, col_num, points) { // set up vertex buffer for webgl
+function init_vbuff(gl, vert_num, col_num, points, n) { // set up vertex buffer for webgl
+    let vbuff = gl.createBuffer(), f_points = new Float32Array(points);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbuff);
+    gl.bufferData(gl.ARRAY_BUFFER, f_points, gl.STATIC_DRAW);
+    let B_SIZE = f_points.BYTES_PER_ELEMENT;
 
-    let pc_buff = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, pc_buff);
-    gl.bufferData(gl.ARRAY_BUFFER, points, gl.STATIC_DRAW);
+    a_position = gl.getAttribLocation(gl.program, 'a_pos');
+    gl.vertexAttribPointer(a_position, n_vert, gl.FLOAT, false, B_SIZE*size_mult, 0); // send a_pos val to shader
+    gl.disableVertexAttribArray(a_position);
+    gl.enableVertexAttribArray(a_position);
 
-    let B_SIZE = points.BYTES_PER_ELEMENT;
-
-    a_pos = gl.getAttribLocation(gl.program, 'a_pos');
-    gl.vertexAttribPointer(a_pos, 3, gl.FLOAT, false, B_SIZE*6, 0);
-    gl.enableVertexAttribArray(a_pos);
-
-    a_col = gl.getAttribLocation(gl.program, 'a_col');
-    gl.vertexAttribPointer(a_col, col_num, gl.FLOAT, false, B_SIZE*6, B_SIZE*3);
-    gl.enableVertexAttribArray(a_col);
+    a_color = gl.getAttribLocation(gl.program, 'a_col');
+    gl.vertexAttribPointer(a_color, col_num, gl.FLOAT, false, B_SIZE*size_mult, B_SIZE*vert_num); // send a_col val to shader
+    gl.disableVertexAttribArray(a_color);
+    gl.enableVertexAttribArray(a_color);
 
     return n;
 }
